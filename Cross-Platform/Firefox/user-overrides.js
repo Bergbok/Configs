ff946163c901a2a2f1a452043edca54e2ee011fc
Bergbok <66174189+Bergbok@users.noreply.github.com>
2024-11-23 13:09:36 +0200

    chore(firefox): add Arkenfox overrides

---
+++ Cross-Platform/Firefox/user-overrides.js
@@ -0,0 +1,62 @@
+// https://arkenfox.github.io/gui/
+
+user_pref("browser.display.background_color.dark", "#212121");
+user_pref("browser.display.background_color", "#212121");
+user_pref("browser.download.autohideButton", false);
+user_pref("browser.toolbars.bookmarks.showOtherBookmarks", false);
+user_pref("browser.toolbars.bookmarks.visibility", "always");
+user_pref("devtools.chrome.enabled", true);
+// user_pref("devtools.debugger.prompt-connection", false);
+user_pref("devtools.theme", "dark");
+user_pref("devtools.toolbox.host", "right");
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+user_pref("browser.startup.page", 3);                                           // 0102
+user_pref("browser.sessionstore.privacy_level", 0);                             // 0103
+user_pref("browser.search.suggest.enabled", true);                              // 0803
+user_pref("browser.urlbar.suggest.searches", true);                             // 0803
+user_pref("browser.formfill.enable", true);                                     // 0810
+user_pref("browser.search.separatePrivateDefault", false);                      // 0830
+user_pref("browser.search.separatePrivateDefault.ui.enabled", false);           // 0830
+// user_pref("security.cert_pinning.enforcement_level", 1);                     // 1223
+user_pref("dom.security.https_only_mode", false);                               // 1244
+user_pref("dom.security.https_only_mode_pbm", false);                           // 1244
+user_pref("dom.security.https_only_mode.upgrade_local", false);                 // 1245
+user_pref("privacy.userContext.newTabContainerOnLeftClick.enabled", false);     // 1702
+user_pref("devtools.debugger.remote-enabled", true);				            // 2608
+user_pref("browser.download.useDownloadDir", true);                             // 2651
+user_pref("browser.download.always_ask_before_handling_new_types", false);      // 2654
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);                        // 2810
+user_pref("privacy.clearOnShutdown.cache", false);                              // 2811
+user_pref("privacy.clearOnShutdown_v2.cache", false);                           // 2811
+user_pref("privacy.clearOnShutdown.downloads", false);                          // 2811
+// user_pref("privacy.clearOnShutdown.formdata", true);                         // 2811
+user_pref("privacy.clearOnShutdown.history", false);                            // 2811
+// user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false);  // 2811
+// user_pref("privacy.clearOnShutdown.siteSettings", false);                    // 2811
+// user_pref("privacy.clearOnShutdown_v2.siteSettings", false);                 // 2811
+// user_pref("privacy.clearOnShutdown.openWindows", false);                     // 2812
+// user_pref("privacy.clearOnShutdown.cookies", false);                         // 2815
+// user_pref("privacy.clearOnShutdown.offlineApps", false);                     // 2815
+// user_pref("privacy.clearOnShutdown.sessions", false);                        // 2815
+// user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);            // 2815
+user_pref("privacy.clearSiteData.cache", false);                                // 2820
+user_pref("privacy.clearSiteData.cookiesAndStorage", false);                    // 2820
+user_pref("privacy.clearSiteData.historyFormDataAndDownloads", false);          // 2820
+user_pref("privacy.clearSiteData.siteSettings", false);                         // 2820
+user_pref("privacy.clearHistory.historyFormDataAndDownloads", false);           // 2830
+user_pref("privacy.sanitize.timeSpan", 1);                                      // 2840
+user_pref("privacy.resistFingerprinting", false);                               // 4501
+user_pref("privacy.resistFingerprinting.pbmode", false);                        // 4501
+// user_pref("browser.display.use_system_colors", true);				        // 4510
+user_pref("browser.privatebrowsing.autostart", false);                          // 5001
+user_pref("signon.rememberSignons", false);                                     // 5003
+user_pref("browser.chrome.site_icons", true);                                   // 5006
+user_pref("browser.urlbar.suggest.history", true);                              // 5010
+user_pref("browser.urlbar.suggest.bookmark", true);                             // 5010
+user_pref("browser.urlbar.suggest.openpage", true);                             // 5010
+user_pref("browser.urlbar.suggest.topsites", true);                             // 5010
+user_pref("browser.urlbar.autoFill",true);                                      // 5012
+// user_pref("browser.download.folderList", 1);                                 // 5016
+user_pref("network.dns.disableIPv6", false);                                    // 5509
