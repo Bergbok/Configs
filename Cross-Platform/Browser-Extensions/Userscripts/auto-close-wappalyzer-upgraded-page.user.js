505ae771b35abfc533e300b214301fff33d2d7d2
Bergbok <66174189+Bergbok@users.noreply.github.com>
2024-11-23 13:06:29 +0200

    chore(browser-ext): add userscript
    Automatically closes the page Wappalyzer opens when it updates.

---
+++ Cross-Platform/Browser-Extensions/Userscripts/auto-close-wappalyzer-upgraded-page.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Auto-close Wappalyzer upgraded page
+// @namespace   Violentmonkey Scripts
+// @match       https://www.wappalyzer.com/upgraded/
+// @grant       none
+// @version     1.0
+// @author      Bergbok
+// @description 2024/09/25, 11:22:37
+// ==/UserScript==
+window.close();
